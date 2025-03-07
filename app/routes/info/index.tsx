e5cd34598dbd22e2f7b6a33846f8845762cc4152
Chris <kispi@naver.com>
2025-03-07 16:54:53 +0900

    fix: change document title when character changes

--- app/routes/info/index.tsx
+++ app/routes/info/index.tsx
@@ -14,6 +14,17 @@ import PanelHexaSkills from '~/components/maplestory/panel-hexa-skills/PanelHexa
 import SearchCharacter from '~/components/maplestory/search-character/SearchCharacter'
 import StoredCharacters from '~/components/maplestory/stored-characters/StoredCharacters'
 
+const createMetaTitle = (c: CharacterInfo) => {
+  if (!c) return '에브리메이플'
+
+  let title = `${c.basic.character_name}`
+  if (!c.basic.character_guild_name) title += ` (${c.basic.world_name})`
+  else title += ` (${c.basic.world_name}@${c.basic.character_guild_name})`
+  title += ' - 에브리메이플'
+
+  return title
+}
+
 export const shouldRevalidate = () => false
 
 export const loader = async ({ request }: { request: Request }) => {
@@ -37,11 +48,7 @@ export const meta: MetaFunction<typeof loader> = ({ data }) => {
   const c = data?.preparedCharacter
   if (!c) return []
 
-  let title = `${c.basic.character_name}`
-  if (!c.basic.character_guild_name) title += ` (${c.basic.world_name})`
-  else title += ` (${c.basic.world_name}@${c.basic.character_guild_name})`
-  title += ' - 에브리메이플'
-
+  const title = createMetaTitle(c)
   const desc = `Lv.${c.basic.character_level} ${c.basic.character_class}`
 
   return [
@@ -74,6 +81,12 @@ const Index = () => {
       navigate('/', { replace: true })
     }
   }, [])
+
+  useEffect(() => {
+    if (!currentCharacter) return
+
+    document.title = createMetaTitle(currentCharacter)
+  }, [currentCharacter])
 
   // SearchCharacter에서 따로 $http 요청하지 말고, url이 바뀌면 loader가 자동 호출되는 것을 이용해서 그냥 그 데이터를 쓰는 형태로 바꿔야 할 듯
   return (
